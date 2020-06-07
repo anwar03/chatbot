e466226fcd8e6482bddde33e82c85c3b5abdb121
anwar03 <anwarazim03@gmail.com>
2020-06-07 15:02:56 +0600

    Initialize echobot.

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from botbuilder.schema import Activity
+from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings
+import asyncio
+import json
+from .echobot import EchoBot
+
+botadaptersettings = BotFrameworkAdapterSettings("", "")
+botadapter = BotFrameworkAdapter(botadaptersettings)
+loop = asyncio.get_event_loop()
+ebot = EchoBot()
+
+@csrf_exempt
+def messages(request):
+
+    if request.method == "POST":
+        jsonmessage = json.loads(request.body)
+        activity = Activity().deserialize(jsonmessage)
+
+        #loop = asyncio.set_event_loop(asyncio.new_event_loop())
+        async def turn_call(turn_context):
+            await ebot.on_turn(turn_context)
+        
+        task = loop.create_task(botadapter.process_activity(activity, "", turn_call))
+        loop.run_until_complete(task)
+
+        return HttpResponse("It's working")
+    else:
+        HttpResponse("Invalid request method")
--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')anltn-5m0*hfd3d$m#qcclux-&du_ql3)jl9*)jdxx7+15l@e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    #user define app
+    'bot',
 ]
 
 MIDDLEWARE = [
